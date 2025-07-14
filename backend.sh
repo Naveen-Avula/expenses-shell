bac7c55346cee4dd1906429a1be27710f243f497
Naveen-Avula <avulanaveen437@gmail.com>
2025-07-14 13:04:25 +0530

    expe

--- backend.sh
+++ backend.sh
@@ -33,7 +33,7 @@ dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
 id expense  &>>$LOGFILE
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then useradd expense &>>$LOGFILE
 VALIDATE $? "Creating expense user"
 else
