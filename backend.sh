f300f24babc51e3949a2f9a7190e3c9539afebf4
Naveen-Avula <avulanaveen437@gmail.com>
2025-07-14 12:44:33 +0530

    expe

--- backend.sh
+++ backend.sh
@@ -14,6 +14,15 @@ then
 else
     echo "You are super user."
 fi
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2... $R FAILURE $N"
+        exit 1
+   else
+       echo -e "$2... $G SUCCESS $N"
+   fi
+}
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabling default nodejs"
 
